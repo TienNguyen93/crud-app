4976b152832b12228437ec3192c2142d30ed2e53
TienNguyen93 <nguyentien9933@gmail.com>
2022-11-30 19:52:33 -0500

    add component folder

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Employee Management System</h1>
+      <button>View all tasks</button>
+      <button>View all employees</button>
+    </div>
+  )
+}
+
+export default Home
