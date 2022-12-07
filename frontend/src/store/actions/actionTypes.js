add37c018a69753984183d7200cc1cb037563a15
TienNguyen93 <nguyentien9933@gmail.com>
2022-12-07 18:33:09 -0500

    add store & reducers

---
+++ frontend/src/store/actions/actionTypes.js
@@ -0,0 +1,14 @@
+// ACTION TYPES;
+
+//All departments
+export const FETCH_ALL_EMPLOYEES = "FETCH_ALL_EMPLOYEES";
+
+// Single department
+export const FETCH_EMPLOYEE = "FETCH_EMPLOYEE";
+
+// All employees
+export const FETCH_ALL_TASKS = "FETCH_ALL_TASKS";
+export const EDIT_TASK = "EDIT_TASK";
+
+// Single employee
+export const FETCH_TASK = "FETCH_TASK";
---
+++ frontend/src/store/thunks.js
@@ -0,0 +1,59 @@
+import * as ac from './actions/actionCreators';
+const axios = require('axios');
+
+//PATH (should be where your server is running)
+let path = "http://localhost:5001/api";
+
+// THUNKS
+
+// All employees
+export const fetchAllEmployeesThunk = () => async (dispatch) => {
+  try {
+    let res = await axios.get(`${path}/employees`)
+    dispatch(ac.fetchAllEmployees(res.data))
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+// Single employee
+export const fetchEmployeeThunk = (id) => async (dispatch) => {
+  try {
+    let res = await axios.get(`${path}/employees/${id}`)
+    dispatch(ac.fetchEmployee(res.data))
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+// All tasks
+export const fetchAllTasksThunk = () => async (dispatch) => {
+  try {
+    let res = await axios.get(`${path}/tasks`)
+    dispatch(ac.fetchAllTasks(res.data))
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+// Single task
+export const fetchTaskThunk = (id) => async (dispatch) => {
+  try {
+    let res = await axios.get(`${path}/tasks/${id}`)
+    dispatch(ac.fetchTask(res.data))
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+// Edit task
+export const editTaskThunk = (task) => async (dispatch) => {
+  try {
+    let res = await axios.put(`${path}/tasks/${task.id}`, task)
+    dispatch(ac.editTask(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
