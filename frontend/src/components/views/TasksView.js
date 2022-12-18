3300049e3506aa828e5e23697e23548c40568aea
TienNguyen93 <nguyentien9933@gmail.com>
2022-12-17 23:31:07 -0500

    add navbar and fix minor bug on edit task form

--- frontend/src/components/views/TasksView.js
+++ frontend/src/components/views/TasksView.js
@@ -1,8 +1,8 @@
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
+import NavBar from "../NavBar";
 
 const TasksView = ({ tasks }) => {
-  const navigate = useHistory()
 
   if (!tasks.length) {
     return (
@@ -12,41 +12,57 @@ const TasksView = ({ tasks }) => {
     );
   }
 
-  const clickEdit = () => {
-    navigate.push('/editTask')
-  }
-
   return (
     <div>
-      <h1>Manage Tasks</h1>
+      <NavBar />
       <div className="all-tasks">
-        <h4>Click on Task description to edit</h4>
-        <table>
-          <tbody>
-            <tr>
-              <th>Tasks</th>
-              <th>Description</th>
-              <th>Priority Level</th>
-              <th>Status</th>
-              <th>Delete Task</th>
-            </tr>
-            {tasks.map(task => {
-              return (
-                <tr key={task.id}>
-                  <td>{task.id}</td>
-                  <td>
-                    <Link className="link" to={`/tasks/${task.id}`}>
-                      {task.description}
-                    </Link>
-                  </td>
-                  <td>{task.priority_level}</td>
-                  <td>{task.completion_status}</td>
-                  <td><button>X</button></td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <div className="header">
+          <h1>Manage Tasks</h1>
+          <div className="notice">
+            <h4>Click on Task description to edit!</h4>
+          </div>
+        </div>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th>Tasks</th>
+                <th>Description</th>
+                <th>Priority Level</th>
+                <th>Status</th>
+                <th>Delete Task</th>
+              </tr>
+              {tasks.map(task => {
+                return (
+                  <tr key={task.id}>
+                    <td>{task.id}</td>
+                    <td>
+                      <Link className="link" to={`/tasks/${task.id}`}>
+                        {task.description}
+                      </Link>
+                    </td>
+                    <td>{task.priority_level}</td>
+                    <td>{task.completion_status}</td>
+                    <td>
+                      <button
+                        style={{ backgroundColor: 'red', width: '5rem' }}>
+                        X
+                      </button>
+                      
+                      <Link className="link" to={`/tasks/${task.id}`}>
+                      <button
+                        style={{ backgroundColor: 'royalblue', width: '5rem' }}>
+                        Edit
+                      </button>
+                      </Link>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+
       </div>
 
       <div className="buttons-wrap">
